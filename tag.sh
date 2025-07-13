cf4a21c62b8d9d0e9d8e649721d793da77e0bc89
MLNotes2718 <150231238+mlnotes2718@users.noreply.github.com>
2025-07-13 17:36:55 +0800

    update tag script

--- tag.sh
+++ tag.sh
@@ -1,11 +1,5 @@
 export VERSION=v0.1.0
 
-# 1. Update the file (search/replace version string)
-#sed -i 's/^version = ".*"/version = "'"$VERSION"'"/' pyproject.toml
-
-# 2. Stage it
-git add pyproject.toml               # ← critical line
-
 # 3. Commit & tag
 git commit -m "chore(release): $VERSION"
 git tag -a "$VERSION" -m "Release $VERSION"
